472dbd885eac855c5d5c6a2863f20eaa71c76d45
Ahsan AK <ahsan.aviator@gmail.com>
2017-03-02 19:52:23 +0500

    Installed NativeBase, build login page using NativeBase Components

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import { View, TouchableHighlight, Text } from 'react-native';
+import { Image } from 'react-native';
 import { LoginStyles } from 'FinanceBakerZ/src/components/login/LoginStyle'
+import {Button, Container, Content, Form, Item, Input, Label, Text} from 'native-base';
 
 export default class Login extends Component {
     render() {
         return (
-            <View style={LoginStyles.container}>
-                <Text>
-                    Route: Login
-                </Text>
-                <TouchableHighlight
-                    onPress={this.props.onPushRoute.bind(null, {type: 'push', key: 'register'})}>
-                    <Text>
-                        Register
-                    </Text>
-                </TouchableHighlight>
-            </View>
-        );
+          <Container>
+            <Content>
+              <Image source={require('FinanceBakerZ/src/images/app-background.png')} style={LoginStyles.backgroundImage} >
+                <Content contentContainerStyle={{alignItems: 'center'}}  style={{marginTop: 100 }} >
+                  <Image source={require('FinanceBakerZ/src/images/logo-final.png')} style={LoginStyles.logo} />
+                </Content>
+                <Content style={{marginTop: 50}} >
+                  <Form>
+                    <Item floatingLabel last>
+                      <Label>Username</Label>
+                      <Input />
+                    </Item>
+                    <Item floatingLabel last>
+                      <Label>Password</Label>
+                      <Input />
+                    </Item>
+                    <Text style={{textAlign: 'right', marginTop: 20, marginBottom: 20}}>Forgot Password</Text>
+                    <Button success full large>
+                      <Text>SIGN IN</Text>
+                    </Button>
+                  </Form>
+                </Content>
+              </Image>
+            </Content>
+          </Container>
+          );
     }
 }
 
+/*
+
+<TouchableHighlight
+onPress={this.props.onPushRoute.bind(null, {type: 'push', key: 'register'})}>
+<Text>
+  Register
+</Text>
+</TouchableHighlight >
+*/
--- src/components/login/LoginStyle.js
+++ src/components/login/LoginStyle.js
@@ -1,8 +1,15 @@
 import {StyleSheet} from 'react-native';
 
 export const LoginStyles = StyleSheet.create({
-    container: {
-        backgroundColor: 'green',
-        flex: 1
-    }
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+    width: null,
+    height: null
+  },
+  logo: {
+    resizeMode: 'center',
+    width: 200,
+    height: 200,
+  }
 });

a6006106e29551bb3d9e3adee574bd2af2a30ee4
Furqan Khanzada <furqan.khanzada@gmail.com>
2017-03-03 16:51:08 +0500

    header imp

---
+++ src/components/header/header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { NavigationExperimental } from 'react-native';
+import { HeaderStyle } from 'FinanceBakerZ/src/components/header/HeaderStyle'
+
+const {
+    Header: NavigationHeader,
+    } = NavigationExperimental;
+
+export default class Header extends Component {
+    _back () {
+        this.props.navigate('pop');
+    }
+    _renderTitleComponent (props) {
+        return (
+            <NavigationHeader.Title>
+                {props.scene.route.key}
+            </NavigationHeader.Title>
+        );
+    }
+    render() {
+        return (
+            <NavigationHeader
+                {...this.props}
+                renderTitleComponent={this._renderTitleComponent.bind(this)}
+                onNavigateBack={this._back.bind(this)}
+                />
+        );
+    }
+}
+

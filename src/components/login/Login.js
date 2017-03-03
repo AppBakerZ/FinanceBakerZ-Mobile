c0f7aed81ca25abde7f525c9ed9d5b480699460b
Furqan Khanzada <furqan.khanzada@gmail.com>
2017-03-03 15:33:59 +0500

    updated routing function

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -6,41 +6,41 @@ import { Button, Container, Content, Form, Item, Input, Label, Text} from 'nativ
 export default class Login extends Component {
     render() {
         return (
-        <Image source={require('FinanceBakerZ/src/images/app-background.png')} style={LoginStyles.backgroundImage} >
-          <Container>
-            <Content>
-                <Content contentContainerStyle={{alignItems: 'center'}}  style={{marginTop: 100 }} >
-                  <Image source={require('FinanceBakerZ/src/images/logo-final.png')} style={LoginStyles.logo} />
-                </Content>
-                  <Content style={{marginTop: 50}} >
-                    <Form>
-                      <Item floatingLabel last>
-                        <Label>Username</Label>
-                        <Input />
-                      </Item>
-                      <Item floatingLabel last>
-                        <Label>Password</Label>
-                        <Input
-                          secureTextEntry
-                        />
-                      </Item>
-                      <Text style={{textAlign: 'right', marginTop: 20, marginBottom: 20, marginRight: 15, fontWeight: 'bold'}}>Forgot Password</Text>
-                      <Button success full large>
-                        <Text>Sign In</Text>
-                      </Button>
-                      <Text style={{textAlign: 'center', marginTop: 20}}>
-                        Don't have an account?
-                        <Text style={{fontWeight: 'bold'}}
-                              onPress={this.props.onPushRoute.bind(null, {type: 'push', key: 'register'})}
-                        >
-                           &nbsp;Sign Up
-                        </Text>
-                      </Text>
-                    </Form>
-                  </Content>
-              </Content>
-          </Container>
-        </Image>
-          );
+            <Image source={require('FinanceBakerZ/src/images/app-background.png')} style={LoginStyles.backgroundImage} >
+                <Container>
+                    <Content>
+                        <Content contentContainerStyle={{alignItems: 'center'}}  style={{marginTop: 100 }} >
+                            <Image source={require('FinanceBakerZ/src/images/logo-final.png')} style={LoginStyles.logo} />
+                        </Content>
+                        <Content style={{marginTop: 50}} >
+                            <Form>
+                                <Item floatingLabel last>
+                                    <Label>Username</Label>
+                                    <Input />
+                                </Item>
+                                <Item floatingLabel last>
+                                    <Label>Password</Label>
+                                    <Input
+                                        secureTextEntry
+                                        />
+                                </Item>
+                                <Text style={{textAlign: 'right', marginTop: 20, marginBottom: 20, marginRight: 15, fontWeight: 'bold'}}>Forgot Password</Text>
+                                <Button success full large>
+                                    <Text>Sign In</Text>
+                                </Button>
+                                <Text style={{textAlign: 'center', marginTop: 20}}>
+                                    Don't have an account?
+                                    <Text
+                                        style={{fontWeight: 'bold'}}
+                                        onPress={this.props.navigate.bind(null, 'push', {key: 'Register'})}>
+                                        Sign Up
+                                    </Text>
+                                </Text>
+                            </Form>
+                        </Content>
+                    </Content>
+                </Container>
+            </Image>
+        );
     }
 }
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -7,47 +7,47 @@ import { RegisterStyles } from 'FinanceBakerZ/src/components/register/RegisterSt
 export default class Register extends Component {
     render() {
         return (
-          <Image source={require('FinanceBakerZ/src/images/app-background.png')} style={RegisterStyles.backgroundImage} >
-            <Container>
-                <Header noShadow style={{backgroundColor: 'transparent', marginTop: 20}}>
-                  <Left>
-                    <Button transparent onPress={this.props.onPopRoute}>
-                      <Icon name='arrow-back' style={{color: 'black', fontSize: 40}} />
-                    </Button>
-                  </Left>
-                </Header>
-                  <Text style={{fontSize: 45, marginTop: 20, marginLeft: 30, color: 'green', fontWeight: 'bold'}}>Sign Up</Text>
-                <Content>
-                  <Form style={{marginTop: 45}}>
-                    <Item floatingLabel last>
-                      <Label style={{marginLeft: 25}}>Name</Label>
-                      <Input style={{marginLeft: 23}}/>
-                    </Item>
-                    <Item floatingLabel last>
-                      <Label  style={{marginLeft: 25}}>Email</Label>
-                      <Input
-                        keyboardType="email-address"
-                        style={{marginLeft: 23}}
-                      />
-                    </Item>
-                    <Item floatingLabel last>
-                      <Label  style={{marginLeft: 25}}>Password</Label>
-                      <Input
-                        style={{marginLeft: 23}}
-                        secureTextEntry
-                      />
-                    </Item>
-                    <Button block large success style={{marginTop: 125}} >
-                      <Text>Sign Up</Text>
-                    </Button>
-                    <Text style={{marginTop: 20, textAlign: 'center'}}>
-                      <Text>Already have an account?</Text>
-                      <Text style={{fontWeight: 'bold'}} onPress={this.props.onPopRoute}> Sign In</Text>
-                    </Text>
-                  </Form>
-                </Content>
-            </Container>
-          </Image>
+            <Image source={require('FinanceBakerZ/src/images/app-background.png')} style={RegisterStyles.backgroundImage} >
+                <Container>
+                    <Header noShadow style={{backgroundColor: 'transparent', marginTop: 20}}>
+                        <Left>
+                            <Button transparent onPress={this.props.navigate.bind(null, 'pop')}>
+                                <Icon name='arrow-back' style={{color: 'black', fontSize: 40}} />
+                            </Button>
+                        </Left>
+                    </Header>
+                    <Text style={{fontSize: 45, marginTop: 20, marginLeft: 30, color: 'green', fontWeight: 'bold'}}>Sign Up</Text>
+                    <Content>
+                        <Form style={{marginTop: 45}}>
+                            <Item floatingLabel last>
+                                <Label style={{marginLeft: 25}}>Name</Label>
+                                <Input style={{marginLeft: 23}}/>
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label  style={{marginLeft: 25}}>Email</Label>
+                                <Input
+                                    keyboardType="email-address"
+                                    style={{marginLeft: 23}}
+                                    />
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label  style={{marginLeft: 25}}>Password</Label>
+                                <Input
+                                    style={{marginLeft: 23}}
+                                    secureTextEntry
+                                    />
+                            </Item>
+                            <Button block large success style={{marginTop: 125}} >
+                                <Text>Sign Up</Text>
+                            </Button>
+                            <Text style={{marginTop: 20, textAlign: 'center'}}>
+                                <Text>Already have an account?</Text>
+                                <Text style={{fontWeight: 'bold'}} onPress={this.props.navigate.bind(null, 'pop')}> Sign In</Text>
+                            </Text>
+                        </Form>
+                    </Content>
+                </Container>
+            </Image>
 
         );
     }

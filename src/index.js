3551777b294093e11fffdcbe5543990172c8b8f8
Ahsan AK <ahsan.aviator@gmail.com>
2017-03-13 11:42:49 +0500

    added splash screen and launcher icon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import {Auth, Drawer} from './Routes.js';
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import SplashScreen from 'react-native-smart-splash-screen';
 
 import Meteor, { createContainer } from 'react-native-meteor';
 
 const SERVER_URL = 'ws://development-financebakerz.herokuapp.com/websocket';
 
 class FinanceBakerZ extends Component {
+
+  componentDidMount () {
+    //SplashScreen.close(SplashScreen.animationType.scale, 850, 500)
+    SplashScreen.close({
+      animationType: SplashScreen.animationType.scale,
+      duration: 850,
+      delay: 500,
+    })
+  }
+
     componentWillMount() {
         Meteor.connect(SERVER_URL);
     }

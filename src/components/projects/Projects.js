f0c842186f9afddc79526da5f14d2e64f37a8f03
Ahsan AK <ahsan.aviator@gmail.com>
2017-03-17 19:22:42 +0500

    made tabNavigator in Projects.js and added title in  navigation header

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 import { ProjectsStyles } from 'FinanceBakerZ/src/components/projects/ProjectsStyle';
 import ViewContainer from 'FinanceBakerZ/src/components/viewContainer/viewContainer';
+import ProjectTabScreen from 'FinanceBakerZ/src/components/projects/ProjectTabScreen';
+import { TabNavigator } from 'react-navigation';
 
 export default class Projects extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {
 
-        };
-    }
-    
+    };
+  }
 
-    render() {
-        const { navigate } = this.props.navigation;
 
-        return (
-            <ViewContainer>
-                <Text>Projects</Text>
-            </ViewContainer>
-        );
-    }
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <ViewContainer>
+        <ProjectTabNavigator />
+      </ViewContainer>
+    );
+  }
 }
+
+
+const ProjectTabNavigator = TabNavigator({
+  'ALL': {
+    screen: ProjectTabScreen
+  },
+  'THIS WEEK': {
+    screen: ProjectTabScreen
+  },
+  'THIS MONTH': {
+    screen: ProjectTabScreen
+  },
+  'THIS YEAR': {
+    screen: ProjectTabScreen
+  }
+}, {
+  tabBarOptions: {
+    style: {
+      backgroundColor: '#DADADA',
+    },
+    labelStyle: {
+      fontFamily: 'QuicksandBook-Regular'
+    },
+    indicatorStyle: {
+      backgroundColor: 'transparent'
+    },
+    tabStyle: {
+      borderLeftColor: '#CFCFCF',
+      borderLeftWidth: 1,
+      borderRightWidth: 1,
+      borderRightColor: '#CFCFCF'
+    },
+    activeTintColor: 'white',
+    inactiveTintColor: 'black',
+    activeBackgroundColor: '#fff',
+    inactiveBackgroundColor : '#DADADA',
+  }
+});
--- src/components/projects/ProjectsStyle.js
+++ src/components/projects/ProjectsStyle.js
@@ -2,4 +2,45 @@ import {StyleSheet} from 'react-native';
 
 export const ProjectsStyles = StyleSheet.create({
 
+
+  listViewContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+
+  listViewContentLeft: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 20,
+    paddingBottom: 20,
+    alignItems: 'center',
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1
+  },
+
+  listViewContentRight:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1
+  },
+
+  icons: {
+    flex: 1,
+    textAlign: 'center'
+  },
+
+  iconText: {
+    flex: 3,
+    fontFamily: 'QuicksandBook-Regular'
+
+  },
+
+  contentRightText: {
+    paddingRight: 10,
+    fontFamily: 'QuicksandBook-Regular'
+
+  }
+
 });

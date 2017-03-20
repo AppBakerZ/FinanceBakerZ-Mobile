b1bdcfe10a1f10833561f570c2ee114682068093
furqan khanzada <furqan.khanzada@gmail.com>
2017-03-20 15:54:54 +0500

    remaining amount implemented

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,101 +5,114 @@ import ViewContainer from 'FinanceBakerZ/src/components/viewContainer/viewContai
 import DashboardTabScreen from 'FinanceBakerZ/src/components/dashboard/DashboardTabScreen';
 import DashboardTabBottomScreen from 'FinanceBakerZ/src/components/dashboard/DashboardTabBottomScreen';
 import { TabNavigator, TabView } from 'react-navigation';
-
+import Meteor, { createContainer } from 'react-native-meteor';
 
 export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
+        this.state = {
+            availableBalance: null
+        };
+        this.getAvailableBalance();
+    }
+    getAvailableBalance (accounts){
+        accounts = accounts || [];
+        Meteor.call('statistics.availableBalance', {accounts}, (err, ab) => {
+            console.log('err, ab', err, ab);
+            if(ab){
+                this.setState({
+                    availableBalance: ab
+                })
+            }else{
 
-    };
-  }
+            }
+        });
+    }
+    render() {
+        const { navigate } = this.props.navigation;
+        console.log(this.props);
 
-  render() {
-    const { navigate } = this.props.navigation;
-    console.log(this.props);
-
-    return (
-      <ViewContainer>
-        <View style={DashboardStyles.imgContainer}>
-          <Image style={DashboardStyles.img} source={require('FinanceBakerZ/src/images/dashboard/dollars.png')}>
-            <Text style={DashboardStyles.text}>Your Remaining Amount is</Text>
-            <Text style={DashboardStyles.textPrice}>Rs 2,30,000</Text>
-          </Image>
-        </View>
-        <View style={DashboardStyles.dateTabContainer}>
-          <DashboardTabNavigator />
-        </View>
-        <View style={DashboardStyles.bottomTabContainer}>
-          <DashboardBottomTabNavigator />
-        </View>
-      </ViewContainer>
-    );
-  }
+        return (
+            <ViewContainer>
+                <View style={DashboardStyles.imgContainer}>
+                    <Image style={DashboardStyles.img} source={require('FinanceBakerZ/src/images/dashboard/dollars.png')}>
+                        <Text style={DashboardStyles.text}>Your Remaining Amount is</Text>
+                        <Text style={DashboardStyles.textPrice}>{this.state.availableBalance}</Text>
+                    </Image>
+                </View>
+                <View style={DashboardStyles.dateTabContainer}>
+                    <DashboardTabNavigator />
+                </View>
+                <View style={DashboardStyles.bottomTabContainer}>
+                    <DashboardBottomTabNavigator />
+                </View>
+            </ViewContainer>
+        );
+    }
 }
 
 const DashboardTabNavigator = TabNavigator({
-  'TODAY': {
-    screen: DashboardTabScreen
-  },
-  'THIS WEEK': {
-    screen: DashboardTabScreen
-  },
-  'THIS MONTH': {
-    screen: DashboardTabScreen
-  },
-  'THIS YEAR': {
-    screen: DashboardTabScreen
-  }
-}, {
-  tabBarComponent: TabView.TabBarTop,
-  lazyLoad: true,
-  swipeEnabled: true,
-  tabBarPosition: 'top',
-  animationEnabled: true,
-  initialRouteName: 'THIS MONTH',
-  tabBarOptions: {
-    style: {
-      backgroundColor: '#3b3b3b',
-      height: 55
+    'TODAY': {
+        screen: DashboardTabScreen
     },
-    labelStyle: {
-      fontFamily: 'QuicksandBold-Regular',
-      fontSize: 12,
-      color: '#ffffff',
+    'THIS WEEK': {
+        screen: DashboardTabScreen
+    },
+    'THIS MONTH': {
+        screen: DashboardTabScreen
+    },
+    'THIS YEAR': {
+        screen: DashboardTabScreen
     }
-  }
+}, {
+    tabBarComponent: TabView.TabBarTop,
+    lazyLoad: true,
+    swipeEnabled: true,
+    tabBarPosition: 'top',
+    animationEnabled: true,
+    initialRouteName: 'THIS MONTH',
+    tabBarOptions: {
+        style: {
+            backgroundColor: '#3b3b3b',
+            height: 55
+        },
+        labelStyle: {
+            fontFamily: 'QuicksandBold-Regular',
+            fontSize: 12,
+            color: '#ffffff',
+        }
+    }
 });
 
 const DashboardBottomTabNavigator = TabNavigator({
-  'TRANSACTIONS': {
-    screen: DashboardTabBottomScreen
-  },
-  'INCOMES': {
-    screen: DashboardTabBottomScreen
-  },
-  'EXPENSES': {
-    screen: DashboardTabBottomScreen
-  }
-}, {
-  tabBarComponent: TabView.TabBarTop,
-  lazyLoad: true,
-  swipeEnabled: true,
-  tabBarPosition: 'top',
-  animationEnabled: true,
-  initialRouteName: 'TRANSACTIONS',
-  tabBarOptions: {
-    style: {
-      backgroundColor: '#3b3b3b',
-      height: 55
+    'TRANSACTIONS': {
+        screen: DashboardTabBottomScreen
     },
-    labelStyle: {
-      fontFamily: 'QuicksandBold-Regular',
-      fontSize: 11,
-      color: '#ffffff',
-      marginTop: 15
+    'INCOMES': {
+        screen: DashboardTabBottomScreen
+    },
+    'EXPENSES': {
+        screen: DashboardTabBottomScreen
     }
-  }
+}, {
+    tabBarComponent: TabView.TabBarTop,
+    lazyLoad: true,
+    swipeEnabled: true,
+    tabBarPosition: 'top',
+    animationEnabled: true,
+    initialRouteName: 'TRANSACTIONS',
+    tabBarOptions: {
+        style: {
+            backgroundColor: '#3b3b3b',
+            height: 55
+        },
+        labelStyle: {
+            fontFamily: 'QuicksandBold-Regular',
+            fontSize: 11,
+            color: '#ffffff',
+            marginTop: 15
+        }
+    }
 });
 

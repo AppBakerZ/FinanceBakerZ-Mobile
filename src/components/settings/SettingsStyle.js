6e4401b713da426d04db6ff49306e590e804722b
Arsalan <arsalan_qamar14@yahoo.com>
2017-04-01 15:19:40 +0500

    Layout implement and get data dynamic.

--- src/components/settings/SettingsStyle.js
+++ src/components/settings/SettingsStyle.js
@@ -1,5 +1,36 @@
 import {StyleSheet} from 'react-native';
 
 export const SettingsStyles = StyleSheet.create({
-
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        width: null,
+        height: null
+    },
+    contentContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 15
+    },
+    borderBottom: {
+        borderBottomWidth: 2,
+        borderBottomColor: '#dadada'
+    },
+    headingText: {
+        marginVertical: 10,
+        color: '#008142',
+        fontFamily: 'QuicksandBold-Regular',
+        fontWeight: '500'
+    },
+    contentText: {
+        marginVertical: 10,
+        fontFamily: 'QuicksandBook-Regular',
+        color: '#333'
+    },
+    row: {
+        flexDirection: 'row'
+    },
+    notificationRadio: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
 });

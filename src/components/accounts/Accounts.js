19538e17ec6ef5033f531092795bd0a4bf1e0a55
Ahsan AK <ahsan.aviator@gmail.com>
2017-04-07 19:52:24 +0500

    fixed issue of icon, get dynamic amountBalance of each account

--- src/components/accounts/Accounts.js
+++ src/components/accounts/Accounts.js
@@ -5,53 +5,71 @@ import ViewContainer from 'FinanceBakerZ/src/components/viewContainer/viewContai
 import Meteor, { createContainer } from 'react-native-meteor';
 import BankIcon from 'FinanceBakerZ/src/icons/BankIcon';
 import { getTheme } from 'react-native-material-kit';
+import {currencyStandardFormat, loggedUserCurrency, alterIconName} from 'FinanceBakerZ/src/customLibrary';
+import CurrencyIcon from 'FinanceBakerZ/src/icons/CurrencyIcon';
+import Loader from 'FinanceBakerZ/src/components/loader/Loader';
+
 
 const theme = getTheme();
 
 class Accounts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    accounts(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      availableBalance: [],
+      loading: true
+    };
+    this.getAvailableBalance = this.getAvailableBalance.bind(this);
+    props.accounts.forEach(account => this.getAvailableBalance([account._id]))
+  }
 
-        return this.props.accounts.map((accountData, i) => {
-            let icon_name = accountData.bank.replace('bank-' , "");
-            return(
-                <View style={[theme.cardStyle, AccountsStyles.card]} >
+  getAvailableBalance (accounts){
+    accounts = accounts || [];
+    Meteor.call('statistics.availableBalance', {accounts}, (err, ab) => {
+      this.setState({availableBalance: [...this.state.availableBalance, ab ? ab : 0]});
+      this.state.availableBalance.length == this.props.accounts.length ? this.setState({loading: false}) : ''
+    });
+  }
 
-                    <View style={AccountsStyles.imgBox}>
-                        <BankIcon name ={icon_name} size={50} />
-                    </View>
-                    <View style={AccountsStyles.detailBox}>
-                        <Text style={AccountsStyles.icons}>{icon_name}</Text>
-                        <Text style={AccountsStyles.accNo}>{accountData.number}</Text>
-                        <Text style={AccountsStyles.amount}>43,900</Text>
-                    </View>
-                </View>
-            )
-        })
-    }
-    render() {
-        const { navigate } = this.props.navigation;
+  accounts(){
+    return this.props.accounts.map((accountData, i) => {
+      let icon_name = accountData.bank ? accountData.bank.replace('bank-' , "") : '';
+      return(
+        <View style={[theme.cardStyle, AccountsStyles.card]} key={i} elevation={3}>
+          <View style={AccountsStyles.imgBox}>
+            <BankIcon name ={icon_name} size={50} />
+          </View>
+          <View style={AccountsStyles.detailBox}>
+            <Text style={AccountsStyles.text}>{icon_name.split('-').map(bankName => bankName  + ' ')}</Text>
+            <Text style={AccountsStyles.accNo}>{accountData.number}</Text>
+            <View style={AccountsStyles.currencyAndAmount}>
+              <CurrencyIcon name={alterIconName(loggedUserCurrency())} size={15} />
+              <Text style={AccountsStyles.amount}>{currencyStandardFormat(this.state.availableBalance[i])}</Text>
+            </View>
+          </View>
+        </View>
+      )
+    })
+  }
+  render() {
 
-                return (
-                        <ViewContainer>
-                            <ScrollView style={AccountsStyles.scroll}>
-                            {
-                              this.accounts()
-                            }
-                            </ScrollView>
-                        </ViewContainer>
-                );
-    }
+    return (
+      <ViewContainer>
+        {!this.state.loading ? <ScrollView style={AccountsStyles.scroll}>
+            {
+              this.accounts()
+            }
+          </ScrollView>: <View style={AccountsStyles.loaderView}><Loader size={35} color="#008142" /></View>}
+      </ViewContainer>
+    );
+  }
 }
 
 export default createContainer(() => {
-    const accountHandler = Meteor.subscribe('accounts');
+  const accountHandler = Meteor.subscribe('accounts');
 
-    return {
-        accountsReady: accountHandler.ready(),
-        accounts: Meteor.collection('accounts').find({})
-    };
+  return {
+    accountsReady: accountHandler.ready(),
+    accounts: Meteor.collection('accounts').find({})
+  };
 }, Accounts);
--- src/components/accounts/AccountsStyle.js
+++ src/components/accounts/AccountsStyle.js
@@ -1,45 +1,64 @@
 import {StyleSheet} from 'react-native';
 
 export const AccountsStyles = StyleSheet.create({
-    box: {
-        height: 150,
-        marginTop: 20,
-        marginLeft: 20,
-        marginRight:20,
-        borderColor: '#cdcdcd',
-        borderWidth: 1,
-        flexDirection: 'row',
-    },
-    imgBox: {
-        flex: 1,
+  box: {
+    height: 150,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight:20,
+    borderColor: '#cdcdcd',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  imgBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  detailBox: {
+    flex: 1.5,
+    paddingLeft: 15
+  },
 
-    },
-    detailBox: {
-        flex: 1
-    },
+  accNo: {
+    fontFamily: 'QuicksandBold-Regular',
+    color: 'black'
+  },
+  amount: {
 
-    accNo: {
-        fontFamily: 'QuicksandBold-Regular',
-    },
-    amount: {
+    fontSize: 15,
+    fontFamily: 'QuicksandBold-Regular',
+    color: 'black',
+    paddingLeft: 5
+  },
 
-        fontSize: 15,
-        fontFamily: 'QuicksandBold-Regular',
-    },
-    card: {
-        padding : 15,
-        marginTop: 15,
-        marginLeft: 25,
-        marginRight: 25,
-        flexDirection: 'row',
+  card: {
+    flex: 1,
+    padding : 18,
+    marginTop: 20,
+    marginLeft: 25,
+    marginRight: 25,
+    flexDirection: 'row'
+  },
+  text: {
+    fontFamily: 'QuicksandBook-Regular',
+    color: '#696969'
+  },
+  scroll:{
+    flex :1
+  },
 
-    },
-    text: {
-        fontFamily: 'QuicksandBook-Regular'
-    },
-    scroll:{
-        flex :1
-    },
+  currencyAndAmount: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  loaderView:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 
 
 });

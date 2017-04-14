ae84354872653de846ba63caa544ca4511cd4def
Furqan Khanzada <furqan.khanzada@gmail.com>
2017-04-14 11:30:37 +0500

    reverted projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,71 +1,38 @@
-import React, { Component } from 'react';
-import { View, Text,TouchableOpacity,ListView} from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { View, Text} from 'react-native';
 import { ProjectsStyles } from 'FinanceBakerZ/src/components/projects/ProjectsStyle';
 import ViewContainer from 'FinanceBakerZ/src/components/viewContainer/viewContainer';
-import Icon from 'FinanceBakerZ/src/icons/CustomIcons';
 import { TabNavigator } from 'react-navigation';
+
 import Meteor, { createContainer } from 'react-native-meteor';
 
 class Projects extends Component {
-  constructor(props) {
-    super(props);
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    constructor(props) {
+        super(props);
 
-      let arr = [];
-      for(let i = 1; i <= 25; i++){
-          arr.push('Logo Design & Branding' + i);
-      }
+        this.state = {
 
-      this.state = {
-          dataSource: ds.cloneWithRows(arr),
-      };
-      console.log(this.props,'props');
-  }
-
-    renderRow(rowData){
-        return(
-
-            <View style={ProjectsStyles.listViewContaineritem}>
-              <View style={ProjectsStyles.listViewContentLeft}>
-                <Icon name='checked' color={'red'} style={ProjectsStyles.icons}></Icon>
-                <Text style={ProjectsStyles.iconText}>{rowData}</Text>
-                <Text style={ProjectsStyles.contentRightText}>Rs. 80,000</Text>
-              </View>
-            </View>
-        );
+        };
     }
 
-  render() {
-    const { navigate } = this.props.navigation;
-      console.log(this.props,'props');
-    return (
-      <ViewContainer >
-        <TouchableOpacity style={ProjectsStyles.filterContainer}onPress={()=> {navigate('ProjectSelection')}}>
-         <View  style={ProjectsStyles.filterDiv}>
-            <View style={ProjectsStyles.filterText}>
-              <Text  style={ProjectsStyles.BankText}>Accounts: DIB | HBL | UBL </Text>
-              <Text  style={ProjectsStyles.BankText}>This Week : Mar 14 - Mar 20</Text>
-            </View>
-            <View style={ProjectsStyles.filterIcon}>
-              <Icon name="filter" size={35} />
-            </View>
-        </View>
-        </TouchableOpacity>
-        <View style={ProjectsStyles.listViewContainer} >
-          <ListView
-              dataSource={this.state.dataSource}
-              renderRow={this.renderRow.bind(this)}
-          />
-        </View>
-      </ViewContainer>
-    );
-  }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        console.log('projects :', this.props.projects)
+        return (
+            <ViewContainer>
+            </ViewContainer>
+        );
+    }
 }
 
-export default createContainer(() => {
+export default createContainer((props) => {
+    const projectsHandle = Meteor.subscribe('projects', {
+        limit: 20
+    });
+
     return {
-        projects: Meteor.collection('projects').find({
-            query: {limit: 10}
-        },)
+        projectsReady: projectsHandle.ready(),
+        projects: Meteor.collection('projects').find({})
     };
 }, Projects);
